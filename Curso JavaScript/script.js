af37d8f327dfafe0951b037b74b02cca528ea363
r.a.prestes almeida <rosielprestes@gmail.com>
2021-04-14 21:03:12 -0300

    up

---
+++ Curso JavaScript/script.js
@@ -0,0 +1,31 @@
+document.getElementById("b7").innerHTML = "Curso Javascript";
+
+document.write("algum curso");
+
+window.alert("caixa mensagem");
+
+console.log("mensagem com console.log")
+
+
+// VARIÁVEIS
+
+x = 4;
+y = 2;
+total = x + y;
+
+alert(total);
+
+
+// CONDICIONAIS
+
+const nome = 'rosiel';
+
+// nome = "prestes";
+
+console.log(nome)
+
+// DOM - DOCUMENT OBJECT MODEL
+
+document.getElementById('curso').innerHTML ='cursos JS <button>Enviar</button>';
+
+document.getElementsByClassName("lista")
